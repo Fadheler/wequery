79972b838d51be0386b1fec3a2adfaa9fcdd7c73
Fadhel Bey <fadhelbey@hotmail.fr>
2019-01-31 14:47:13 +0100

    Update wequery.php

--- wequery.php
+++ wequery.php
@@ -1,24 +1,23 @@
 $cnx = new mysqli("host", "user", "password", "database");
-function wequery($q, $vars) {
+function wequery($q, $vars = "") {
 	global $cnx;
 	$stmt = $cnx->prepare($q);
-	$e = explode(',', $vars);
-	$str = '$stmt->bind_param("'.$e[0].'"';
-	for($i = 1; $i < sizeof($e); $i++) {
-		if($e[0][$i] == "i")
-			$str .=', intval($e['.$i.'])';
-		else if($e[0][$i] == "d")
-			$str .=', floatvar($e['.$i.'])';
-		else
-			$str .=', $e['.$i.']';
+	if($vars != "") {
+		$e = explode(',', $vars);
+		$str = '$stmt->bind_param("'.$e[0].'"';
+		for($i = 1; $i < sizeof($e); $i++) {
+			if($e[0][$i-1] == "i")
+				$str .=', intval($e['.$i.'])';
+			else if($e[0][$i-1] == "d")
+				$str .=', floatvar($e['.$i.'])';
+			else
+				$str .=', $e['.$i.']';
+		}
+		$str .=');';
+		eval($str);
 	}
-	$str .=');';
-	echo $str;
-	eval($str);
 	$stmt->execute();
-	$stmt->store_result();
-	$meta = $stmt->result_metadata();
-	$nrows = $stmt->num_rows;
+	$results = $stmt->get_result();
 	$stmt->close();
-	return $meta;
+	return $results;
 }
