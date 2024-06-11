610fb778873aa7a9550149ef8c912bfe5e8be6b1
Fadhel Bey <fadhelbey@hotmail.fr>
2024-06-11 19:28:30 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,7 +1,7 @@
 <?php
   include('wequery.php');
   $brand = "BMW";
-  $q = wequery("SELECT * FROM cars WHERE brand=?", "s, $brand");
+  $q = wequery("SELECT * FROM cars WHERE brand=?", "s", $brand);
   while($row = $q->fetch_assoc()) {
     echo $row['brand'].": ".$row['number']." cars available";
   }
